d1db5699a6183b7f59f56faf5b82fdba54125cbe
chimame <rito.tamata@gmail.com>
2022-05-05 12:11:02 +0900

    Add expo-image-picker

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const [status, requestPermission] = ImagePicker.useCameraPermissions()
+
+  if (!status?.granted) {
+    requestPermission()
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Pick an image from camera" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     </View>
   );
 }
